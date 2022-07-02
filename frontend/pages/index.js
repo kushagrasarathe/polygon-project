cda2fdfb4d86538a1426d061d26421b4a5668ab1
kushagrasarathe <76868364+kushagrasarathe@users.noreply.github.com>
2022-07-02 16:21:46 +0530

    made homepage responsive

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,16 +1,17 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import Card from '../src/components/Card'
-import Navbar from '../src/components/Navbar'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import Card from "../src/components/Card";
+import Navbar from "../src/components/Navbar";
+import styles from "../styles/Home.module.css";
 import music from "../src/assets/music.gif";
-import nft from "../src/assets/nft.svg";
+import nft from "../src/assets/nft.gif";
 import creator from "../src/assets/creator.gif";
-
+import artist from "../src/assets/artist.gif";
+import Hover from "react-3d-hover";
 
 export default function Home() {
   return (
-    <div>
+    <div style={{ scrollBehavior: "smooth" }}>
       <Navbar />
       <div className={styles.main}>
         <p className={styles.subHeading}>
@@ -18,20 +19,63 @@ export default function Home() {
         </p>
         <h1 className={styles.heading}>CAZE</h1>
         <p className={styles.subHeading}>
-          <span className={styles.tagline}>Subscribe to your favorite creators And get access to exclusive content</span>
+          <span className={styles.tagline}>
+            Subscribe to your favorite creators And get access to exclusive
+            content
+          </span>
         </p>
+        <button className={styles.btn} role="button">
+          Explore
+        </button>
       </div>
 
-    {/* <hr /> */}
+      {/* <hr /> */}
 
-      <div className={styles.cards}>
-        <div>
-          <Card music={music} />
-          <Card music={nft} />
-          <Card music={creator} />
+      <div className={styles.grid}>
+        <div className={styles.gridItem}>
+          {/* <Hover scale={1} perspective={900} speed={500}> */}
+            <Card
+              music={music}
+              heading={"Music Artists"}
+              matter={
+                "Support your favorite music creators and pay only for what you listen to 🤩 "
+              }
+            />
+          {/* </Hover> */}
+        </div>
+
+        <div className={styles.gridItem}>
+          {/* <Hover scale={1} perspective={900} speed={500}> */}
+            <Card
+              music={artist}
+              heading={"Digital Creators"}
+              matter={"Access digital arts of creators around the world 😇"}
+            />
+          {/* </Hover> */}
+        </div>
+        <div className={styles.gridItem}>
+          {/* <Hover scale={1} perspective={900} speed={500}> */}
+            <Card
+              music={nft}
+              heading={"NFT Artists"}
+              matter={
+                "Mint NFTs from your favorite projects and artists by only paying once 🫣"
+              }
+            />
+          {/* </Hover> */}
+        </div>
+        <div className={styles.gridItem}>
+          {/* <Hover scale={1} perspective={900} speed={500}> */}
+            <Card
+              music={creator}
+              heading={"Video Creators"}
+              matter={
+                "Support your favorite video creators/ and get access to special content every month 😘	"
+              }
+            />
+          {/* </Hover> */}
         </div>
       </div>
-    
     </div>
-  )
+  );
 }
--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react";
 import styles from "./Card.module.css";
 import Image from "next/image";
 import music from "../assets/music.gif";
@@ -6,7 +6,37 @@ import music from "../assets/music.gif";
 export default function Card(props) {
   return (
     <div className={styles.card}>
-        <a className={styles.image}><Image src={props.music} alt="" /></a>
+      {/* <div className={styles.gBorder}> */}
+
+      <div className={`${styles.content} ${styles.border}`}>
+        <div>
+          <h1 className={styles.cardHeader}>{props.heading}</h1>
+          <p className={styles.cardText}>{props.matter}</p>
+        </div>
+        <a className={styles.image}>
+          <Image src={props.music} alt="" />
+        </a>
+      {/* </div> */}
+      </div>
+      {/* <div className={styles.card}>
+          <div className={styles.content}>
+            <div className={styles.gBorder}>
+              <a className={styles.image}>
+                <Image src={props.music} alt="" />
+              </a>
+            </div>
+          </div>
+        </div> */}
+
+      {/* <div className={styles.container}>
+          <div className={`${styles.item}`}>
+            <div className={`${styles.card}`}>
+              <div className={`${styles.card__content} ${styles.gBorder}`}>
+                
+              </div>
+            </div>
+          </div>
+        </div> */}
     </div>
-  )
+  );
 }
