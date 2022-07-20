862fa9f468c4c2d05aa8dc3406b1bf9cf44eed4c
kushagrasarathe <76868364+kushagrasarathe@users.noreply.github.com>
2022-07-20 17:12:00 +0530

    revamped landing page

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -5,19 +5,30 @@ import music from "../assets/music.gif";
 
 export default function Card(props) {
   return (
-    <div className={`${styles.card}`}>
-      <div className={`${styles.shadow}  ${styles.glow}`}>
-
-      <div className={`${styles.content} ${styles.border}`}>
-        <div>
-          <h1 className={styles.cardHeader}>{props.heading}</h1>
-          <p className={styles.cardText}>{props.matter}</p>
+    <>
+      <div className={styles.category_card}>
+        <div className={`${styles.card_border} `}>
+          <h1 className={styles.card_title}>{props.heading}</h1>
+          <div className={styles.card_image}>
+            <Image src={props.img} alt="" />
+          </div>
+          <p className={styles.card_content}>{props.matter}</p>
         </div>
-        <a className={styles.image}>
-          <Image src={props.img} alt="" />
-        </a>
       </div>
-      </div>
-    </div>
+
+      {/* <div className={`${styles.card}`}>
+        <div className={`${styles.shadow}  ${styles.glow}`}>
+          <div className={`${styles.content} ${styles.border}`}>
+            <div>
+              <h1 className={styles.cardHeader}>{props.heading}</h1>
+              <p className={styles.cardText}>{props.matter}</p>
+            </div>
+            <a className={styles.image}>
+              <Image src={props.img} alt="" />
+            </a>
+          </div>
+        </div>
+      </div> */}
+    </>
   );
 }
--- frontend/src/components/PlanCard.jsx
+++ frontend/src/components/PlanCard.jsx
@@ -1,11 +1,35 @@
 import React from "react";
 import styles from "./PlanCard.module.css";
 import Image from "next/image";
-import Button from '../components/Button'
+import Button from "../components/Button";
 
 export default function PlanCard(props) {
   return (
-    <div className={`${styles.card}`}>
+    <>
+      <div className={` ${styles.plan_card}  ${styles.shadow} `}>
+        <div className={`${styles.card_content}`}>
+          <h2>{props.name}</h2>
+          <br />
+          <div className={styles.card_img}>
+            <Image src={props.img} alt="" />
+          </div>
+          <br />
+          <h3>{props.month}</h3>
+          <p>{props.amount}</p>
+          <br />
+          <div className={styles.plan_benefits}>
+          <h4>Benefits</h4>
+            <ul>
+              <li>lorem</li>
+              <li>lorem</li>
+              <li>lorem</li>
+            </ul>
+          </div>
+          <Button title={"Subscribe"} />
+        </div>
+      </div>
+
+      {/* <div className={`${styles.card}`}>
       <div className={`${styles.shadow}  ${styles.glow}`}>
         <div className={`${styles.content} ${styles.border}`}>
           <div>
@@ -30,6 +54,7 @@ export default function PlanCard(props) {
           </div>
         </div>
       </div>
-    </div>
+    </div> */}
+    </>
   );
 }
