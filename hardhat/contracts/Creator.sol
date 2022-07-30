46aeb11f45456cc6e3349318eefc81a324db93c0
dhruv <dhruvagarwal2017@gmail.com>
2022-07-30 11:43:38 +0530

    Contracts update

--- hardhat/contracts/Creator.sol
+++ hardhat/contracts/Creator.sol
@@ -2,12 +2,20 @@
 pragma solidity ^0.8.4;
 import "@openzeppelin/contracts/access/Ownable.sol";
 
+interface NFT {
+    function mint(address user, uint256 tokenId) external;
+
+    function balanceOf(address user, uint256 tokenId) external;
+}
+
 contract Creator is Ownable {
+    NFT nft;
+
     struct creator {
         address creatorAddress;
         string userData;
         uint256 balance;
-        uint256 subscibers;
+        uint256 subscribers;
     }
     // userData is just a Ipfs CID that has name , description , pfp can be stored here
     // Content is stored in another contract Content.sol
@@ -19,7 +27,13 @@ contract Creator is Ownable {
 
     event creatorAdded(uint256 id, address creator, string user);
 
+    constructor(address _NFTcontract) {
+        nft = NFT(_NFTcontract);
+    }
+
     /// @dev add the creator to the creators array with the Crator struct details
+    /// @dev Checks if the user don't owns any creator NFT before registering
+    /// @dev Mints a NFT after the creator is regsitered
     /// @param _creatorAddress - address of the creator to be added
     /// @param _userData - CID of the creator Data stored on IPFS
     //// @return _id - ID assigned to the creator , can be stored and accessed
@@ -27,9 +41,14 @@ contract Creator is Ownable {
         public
         returns (uint256 _id)
     {
+        require(
+            nft.balanceOf(_creatorAddress, 3) == 0,
+            "You are already a creator"
+        );
         require(_creatorAddress != address(0), "Enter a Valid Address");
         creators[id] = creator(_creatorAddress, _userData, 0, 0);
         uint256 _id = id;
+        nft.mint(_creatorAddress, 3);
         emit creatorAdded(_id, _creatorAddress, _userData);
         id += 1;
         return _id;
@@ -50,6 +69,7 @@ contract Creator is Ownable {
         internal
         returns (uint256 _balance)
     {
+        require(nft.balanceOf(msg.sender, 3) > 0, "You are not a creator");
         creators[_id].balance += _amount;
         return creators[_id].balance;
     }
@@ -59,19 +79,23 @@ contract Creator is Ownable {
     /// @param _amount -  amount to be deducted to the balance
     /// @return _balance -  returns the current balance after decreasing
     function detuctBalance(uint256 _id, uint256 _amount)
-        internal
+        external
         returns (uint256 _balance)
     {
+        require(nft.balanceOf(msg.sender, 3) > 0, "You are not a creator");
         creators[_id].balance -= _amount;
         return creators[_id].balance;
     }
 
     /// @dev -  Adds subscriber to the creator details
     /// @param _id - id of the creator
-    /// @return _subsriber - current subscribers of the creatore
-    function addSubscriber(uint256 _id) internal returns (uint256 _subscriber) {
-        creators[_id].subscibers += 1;
-        return creators[_id].subscibers;
+    /// @return _subsrcibers - current subscribers of the creators
+    function addSubscriber(uint256 _id)
+        external
+        returns (uint256 _subscribers)
+    {
+        creators[_id].subscribers += 1;
+        return creators[_id].subscribers;
     }
 
     /// @dev returns the creator for the id fetched
@@ -91,4 +115,12 @@ contract Creator is Ownable {
     function fetchAddress(uint256 _id) public view returns (address) {
         return creators[_id].creatorAddress;
     }
+
+    // returns the address for the Creator ID
+    function fetchBalance(uint256 _id) public view returns (uint256) {
+        return creators[_id].balance;
+    }
+
+    /// to check to make some functions internal so that nobody else can change these values
+    /// also to check the person calling this function is a creator or not
 }
--- hardhat/contracts/Subscription1.sol
+++ hardhat/contracts/Subscription1.sol
@@ -11,48 +11,66 @@ pragma solidity ^0.8.4;
 interface NFT {
     function mint(address user, uint256 tokenId) external;
 
-    function balanceOf(address user, uint256 tokenId) external;
+    function balanceOf(address user, uint256 tokenId)
+        external
+        returns (uint256);
+}
+
+interface Creator {
+    function addBalance(uint256 _id, uint256 _amount)
+        external
+        returns (uint256 _balance);
+
+    function fetchAddress(uint256 _id) external view returns (address);
+
+    function fetchBalance(uint256 _id) external view returns (uint256);
+
+    function detuctBalance(uint256 _id, uint256 _amount)
+        external
+        returns (uint256 _balance);
 }
 
 contract SubscriptionPlan {
-    // IERC20 token;
     address public owner;
-    // uint256 public frequency;
-    // uint256 public amount ;
-    /// frequency of payment
 
     NFT nft;
 
+    // creator contract to be used for details and all
+    Creator creator;
+
     uint256 totalCreators = 0;
 
+    /// struct for the details of the Plan created and by which Creator it is applicable
     struct Plan {
-        address creator;
         uint256 amount;
         uint256 frequency;
     }
 
     struct Subscription {
-        address subscriber;
         uint256 planId;
         uint256 start;
         uint256 nextPayment;
     }
     /// intialize a subscriptions for a creator
-    /// mapping from user address-->creator address--> Subscription taken
-    mapping(address => mapping(address => Subscription)) public subscriptions;
+    /// mapping from user address-->creator Id--> Subscription taken
+    mapping(address => mapping(uint256 => Subscription)) public subscriptions;
+
+    // mapping from creator Id to array
+    mapping(uint256 => address[]) public subscribers ;
 
     // mapping(address => Plan) public planWithId;
 
     /// mapping from creator --> planID --> Plan
+    /// freedom for the creator to add their own plans
     mapping(address => mapping(uint256 => Plan)) public plans;
+    uint256 planId = 0;
 
     //mapping from the creator id to the creator address
     // marked private for now , so can be accesses only inside the contract
-    mapping(uint256 => address) private creators;
+    // mapping(uint256 => address) private creators;
 
     // mapping from creator address --> balance
-    mapping(address => uint256) balances;
-    uint256 planId = 0;
+    // mapping(address => uint256) balances;
 
     // 0 --> Silver NFT
     // 1 --> Gold NFT
@@ -66,8 +84,9 @@ contract SubscriptionPlan {
     event paymentSent(address from, address to, uint256 amount, uint256 date);
 
     /// costructor to take all the details needed for a subscription plan
-    constructor(address nftContract) payable {
+    constructor(address nftContract, address creatorContract) payable {
         nft = NFT(nftContract);
+        creator = Creator(creatorContract);
         owner = msg.sender;
     }
 
@@ -76,31 +95,34 @@ contract SubscriptionPlan {
         _;
     }
 
-    modifier onlyCreator(uint256 _id) {
-        require(creators[_id] == msg.sender, "You are not a creator");
+    modifier onlyCreator() {
+        require(nft.balanceOf(msg.sender, 3) != 0, "You are not a creator");
         _;
     }
 
-    /// @dev - to addCreator to the platform
-    /// @param _creator - address of the creator to be added
-    /// only owner can call this function
-    function addCreator(address _creator)
-        public
-        onlyOwner
-        returns (uint256 _id)
-    {
-        require(_creator != address(0), "Please enter a valid address");
-        /// check if he is already there or not
-        // creators.push(_creator)
-        creators[totalCreators] = _creator;
-        uint256 id = totalCreators;
-        totalCreators += 1;
-        /// length of the array or the id of creator
-        return id;
-    }
+    // /// @dev - to addCreator to the platform
+    // /// @param _creator - address of the creator to be added
+    // /// only owner can call this function
+    // function addCreator(address _creator)
+    //     public
+    //     onlyOwner
+    //     returns (uint256 _id)
+    // {
+    //     require(_creator != address(0), "Please enter a valid address");
+    //     /// check if he is already there or not
+    //     // creators.push(_creator)
+    //     creators[totalCreators] = _creator;
+    //     uint256 id = totalCreators;
+    //     totalCreators += 1;
+    //     /// length of the array or the id of creator
+    //     return id;
+    // }
 
     /// @dev - to create plans
     /// @param id - id of the Plan created by the Owner , and only they can add a new plan
+    /// @param planAmount - amount to be set for the Plan
+    /// @param frequency - frequency of the plan in days
+    /// @param _creator -  address of the creator for which the plan is added
     function createPlan(
         uint256 id,
         uint256 planAmount,
@@ -109,38 +131,41 @@ contract SubscriptionPlan {
     ) public onlyOwner {
         require(planAmount > 0, "The amount for the plan should be > 0 ");
         require(frequency > 0, "Time peroid should be > 0");
-        plans[msg.sender][id] = Plan(_creator, planAmount, frequency);
+        plans[_creator][id] = Plan(planAmount, frequency);
     }
 
     /// @dev to subscribe for the plan
-    /// @param _creator -  address of the creator to be subscribed for
+    /// @param _creatorID -  address of the creator to be subscribed for
     /// @param _planId - Plan to be subscribed
-    function subscribe(address _creator, uint256 _planId) external payable {
-        Plan memory _plan = plans[_creator][_planId];
+    function subscribe(uint256 _creatorID, uint256 _planId) external payable {
+        /// for now the plans are fetched for the owner of contract , set intially
+        Plan memory _plan = plans[owner][_planId];
         uint256 amount = _plan.amount;
         uint256 _frequency = _plan.frequency;
         require(msg.value == amount, "Incorrect Amount");
         // contract recieved the amount
-        balances[_creator] += amount;
+        creator.addBalance(_creatorID, amount);
+        address _creator = creator.fetchAddress(_creatorID);
         emit paymentSent(msg.sender, _creator, amount, block.timestamp);
-        subscriptions[msg.sender][_creator] = Subscription(
-            msg.sender,
+        subscriptions[msg.sender][_creatorID] = Subscription(
             _planId,
             block.timestamp,
             block.timestamp + _frequency
         );
+        subscribers.push(msg.sender);
         /// will mint the NFT right after the adding to subscriptions list
         nft.mint(msg.sender, _planId);
         emit subscriptionCreated(msg.sender, block.timestamp);
     }
 
     /// @dev to cancel the subscription plan
-    /// @param _creator -  address of the creator for which the plan is to be cancelled
+    /// @param _creatorID -  address of the creator for which the plan is to be cancelled
     /// @param _planId - Id of the plan to be cancelled
-    function cancel(address _creator, uint256 _planId) external {
-        Subscription storage subscription = subscriptions[msg.sender][_creator];
+    function cancel(uint256 _creatorID, uint256 _planId) external {
+        address _creator = creator.fetchAddress(_creatorID);
+        Subscription storage subscription = subscriptions[msg.sender][_creatorID];
         require(
-            subscription.subscriber != address(0),
+            ,
             "This subscription does not exsist"
         );
         require(subscription.planId == _planId, "Incorrect Plan Id");
@@ -151,14 +176,15 @@ contract SubscriptionPlan {
 
     ///@dev - To pay the required amount later
     ///@param subscriber -  The address of the subscriber
-    ///@param _creator - The address of the creator
+    ///@param _creatorID - The address of the creator
     ///@param _planId - Id of the plan selected
     function pay(
         address subscriber,
-        address _creator,
+        uint256 _creatorID,
         uint256 _planId
     ) external payable {
-        Subscription storage subscription = subscriptions[subscriber][_creator];
+        address _creator = creator.fetchAddress(_creatorID);
+        Subscription storage subscription = subscriptions[subscriber][_creatorID];
         require(
             subscription.subscriber != address(0),
             "This subscription does not exsist"
@@ -168,24 +194,20 @@ contract SubscriptionPlan {
         uint256 amount = _plan.amount;
         require(msg.value == amount, "Incorrect amount");
         // eth transferred to the contract
-        balances[_creator] += amount;
+        creator.addBalance(_creatorID, amount);
         emit paymentSent(subscriber, _creator, amount, block.timestamp);
         subscription.nextPayment += _plan.frequency;
     }
 
     /// @dev - Function for creators to withdraw the balance
-    /// @param creator - Address of the creator
-    /// @param creatorId-  Id of the creator to verify if right creator is calling the function or not
+    /// @param _Id Id of the creator to verify if right creator is calling the function or not
     /// @return success -  Returns if the transaction was completed or not
-    function withdraw(address creator, uint256 creatorId)
-        external
-        onlyCreator(creatorId)
-        returns (bool)
-    {
-        uint256 amount = balances[creator];
-        require(amount > 0, "Amount is 0");
-        balances[creator] = 0;
-        (bool success, ) = creator.call{value: amount}("");
+    function withdraw(uint256 _Id) external onlyCreator returns (bool) {
+        uint256 _amount = creator.fetchBalance(_Id);
+        require(_amount > 0, "Amount is 0");
+        ///update the balance here for the creator to 0
+        creator.detuctBalance(_Id, _amount);
+        (bool success, ) = creator.call{value: _amount}("");
         require(success, "Failed to send Ether");
         return success;
     }
@@ -203,19 +225,10 @@ contract SubscriptionPlan {
         return _plan;
     }
 
-    /// @dev - Function to check the balance for the creator
-    /// @param _creator - Address of the creator for which balance is to be fetched
-    /// @return _balance -Balance of the creator
-    function checkBalance(address _creator)
-        public
-        view
-        returns (uint256 _balance)
-    {
-        uint256 balance = balances[_creator];
-        return balance;
-    }
-
-    /// returns the susbcriptions taken by a user according to the creator they have subscribed
+    /// @dev fetches the susbcriptions taken by a user according to the creator they have subscribed
+    /// @param user - user address for whom the details are to be fetched
+    /// @param creator -  the creator address for whom they have subscribed for
+    /// @return Subscription -  details of the subscription
     function getSubscriptions(address user, address creator)
         public
         view
@@ -225,6 +238,12 @@ contract SubscriptionPlan {
         return subscriptionsDone;
     }
 
+    /// @dev - to fetch the subscribers array for the creator 
+    /// @param _id -  id of the creator for whom the address are needed
+    /// @return _subscribers - The array of subscribers for the creator
+    function getSubscribers(uint256 _id) public view returns(address[]) {
+        return subscribers[_id];
+    }
     receive() external payable {}
 
     fallback() external payable {}
